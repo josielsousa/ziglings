0d88b0d3034aa65336e19bf3ea4ff3b34117a5db
Josiel Sousa <josiel.psousa@gmail.com>
2023-02-13 21:15:33 -0300

    chore: arrays manipulation

--- exercises/005_arrays2.zig
+++ exercises/005_arrays2.zig
@@ -25,12 +25,12 @@ pub fn main() void {
     // (Problem 1)
     // Please set this array concatenating the two arrays above.
     // It should result in: 1 3 3 7
-    const leet = ???;
+    const leet = le ++ et;
 
     // (Problem 2)
     // Please set this array using repetition.
     // It should result in: 1 0 0 1 1 0 0 1 1 0 0 1
-    const bit_pattern = [_]u8{ ??? } ** 3;
+    const bit_pattern = [_]u8{ 1, 0, 0, 1 } ** 3;
 
     // Okay, that's all of the problems. Let's see the results.
     //
@@ -44,14 +44,14 @@ pub fn main() void {
     //
     std.debug.print("LEET: ", .{});
 
-    for (leet) |n| {
-        std.debug.print("{}", .{n});
+    for (leet) |e| {
+        std.debug.print("{}", .{e});
     }
 
     std.debug.print(", Bits: ", .{});
 
-    for (bit_pattern) |n| {
-        std.debug.print("{}", .{n});
+    for (bit_pattern) |b| {
+        std.debug.print("{}", .{b});
     }
 
     std.debug.print("\n", .{});
